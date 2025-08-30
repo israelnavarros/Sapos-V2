4ccf495780c11b031e1995b4c3db3a76cb66de80
Israel Navarro <isra.lemes@gmail.com>
2025-08-30 20:05:57 -0300

    add: Mudanças

--- frontend/src/Header.jsx
+++ frontend/src/Header.jsx
@@ -18,8 +18,8 @@ function Header() {
       <nav className="max-w-7xl mx-auto flex items-center justify-between px-4 py-3">
         {/* Logo */}
         <Link to="/" className="flex items-center gap-2">
-          <img src="/src/assets/Logo.png" alt="Logo SAPO" width={50} height={50} className='bg-amber-50' />
-          <span className="font-bold text-xl text-teal-700 ">SAPOS</span>
+          <img src="/src/assets/Logo.png" alt="Logo SAPO" width={50} height={50} className='bg-amber-50 rounded-2xl' />
+          <span className="font-bold text-xl text-white ">SAPOS</span>
         </Link>
 
         {/* Botão do menu mobile */}
