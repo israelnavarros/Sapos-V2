970b74c0a6a5335f4307ee8b9abd0b85d5164bf1
Israel Navarro <isra.lemes@gmail.com>
2025-11-09 18:04:58 -0300

    add: tirando botao errado

--- frontend/src/EstMeuGrupo.jsx
+++ frontend/src/EstMeuGrupo.jsx
@@ -120,15 +120,6 @@ export default function EstMeuPacientes() {
           <div className="flex flex-col md:flex-row justify-between items-center mb-8 gap-4">
             <h1 className="text-3xl font-bold text-gray-900">Meu Grupo de Pacientes</h1>
             <button
-              onClick={() => navigate('/est_adicionar_paciente')}
-              className="flex items-center gap-2 bg-green text-white px-5 py-2.5 rounded-lg font-semibold shadow-md hover:bg-green-600 cursor-pointer transition-transform transform hover:scale-105"
-            >
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                <path fillRule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clipRule="evenodd" />
-              </svg>
-              Adicionar Paciente
-            </button>
-            <button
                 onClick={openModal}
                 className="flex items-center gap-2 bg-yellow-500 text-white px-4 py-2.5 rounded-lg font-semibold shadow-md hover:bg-yellow-600 cursor-pointer transition-transform transform hover:scale-105"
               >
